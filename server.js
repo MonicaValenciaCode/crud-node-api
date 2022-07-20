e0788eeb8b21311618bc3fbb3c4d798b28f249c1
Eder Dominguez <ederdominguez@MacBook-Pro-de-Eder.local>
2022-07-20 16:56:50 -0500

    add env file

--- server.js
+++ server.js
@@ -9,3 +9,5 @@ app.use('/api/', apiRoute);
 app.listen(PORT, ()=>{
     console.log("server list: ", PORT);
 })
+
+//https://www.tutorialrepublic.com/snippets/preview.php?topic=bootstrap&file=user-management-data-table
